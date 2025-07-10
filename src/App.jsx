ec081bd6be525cb73be0b23c047fabf9187ccb56
samuel88 <samuelgide88@gmail.com>
2025-07-10 23:25:49 +0300

    header.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header'
 import Home from './components/Home'
 import Projects from './components/Projects'
 
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
       <Routes>
         <Route path='/' element={ <Home/> } />
         <Route path='/projects' element={ <Projects/> } />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,12 @@ import About from './About'
 import Services from './Services'
 import Skills from './Skills'
 import ContactMe from './ContactMe'
+import Header from './Header'
 
 const Home =()=> {
     
   return <section className="home">
+    <Header/>
     <Hero/>
     <About/>
     <Services/>
