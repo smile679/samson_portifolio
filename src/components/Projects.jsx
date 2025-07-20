d748c586013198d298a758ae4468c3f00f5bc05a
samuel88 <samuelgide88@gmail.com>
2025-07-20 20:28:55 +0300

    project.jsx update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,97 +1,147 @@
 import portfolio from '../images/projectImages/portfolio.webp'
 import movieScope from '../images/projectImages/movieScope.webp'
+import movieScope2 from '../images/projectImages/movieScope2.jpg'
 import foodiehome2 from '../images/projectImages/foodiehome2.webp'
+import foodie2 from '../images/projectImages/foodie2.webp'
+import { useState, useEffect } from 'react'
 
 const Projects = ()=>{
+  const [showText, setShowText] = useState({movie: false, foodie: false})
 
-  return <section className='max-w-6xl mx-auto min-h-screen flex flex-col py-10 px-5 text-center' id='projectId'>
+  useEffect(() => {
+  const isMobile = window.innerWidth < 640;
+  setShowText({ movie: !isMobile, foodie: !isMobile });
+}, []);
+
+  return <section className='max-w-6xl mx-auto min-h-screen flex flex-col py-10 px-5' id='projectId'>
     <h1 className='my-5 text-shadow-gray-800 text-shadow-lg'>Projects</h1>
     <div className='project-card'>
-      <img src={portfolio} alt="portfolio image"/>
-      <div className='project-info'>
         <h3>Portfolio</h3>
-          <p className='text-[13px] leading-5 text-white'>I built this portfolio website entirely from scratch
-            starting with the design and transforming it into a fully functional site. The project is developed using 
-            <span className='text-amber-400 font-bold'> JavaScript</span>, 
-            <span className='text-gray-800 font-bold'> React </span>, and the latest <span className='text-sky-400 font-bold'> Tailwind CSS v4. </span><span className='hidden sm:flex'> It includes a clean Home page, an About section, 
-            a detailed Skills section, and a fully functional Contact form.</span>
-          </p>
-        <div className='project-buttons'>
-          <a
-          href="https://samsongideyportifolio.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="button px-2 py-2 my-2 max-sm:my-3"> Visit Live Website</button>
-        </a>
-        <a
-          href="https://github.com/smile679/samson_portifolio.git"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <button className="button3"> Code on Github</button>
-        </a>
-      </div>
+        <div className='project-grid'>
+          <img src={portfolio} alt="portfolio image"/>
+        <div className='project-info'>
+            <p className='text-[1rem] leading-6 text-white'>I built this portfolio website entirely from scratch
+              starting with the design and transforming it into a fully functional site. The project is developed using 
+              <span className='text-amber-400 font-bold'> JavaScript</span>, 
+              <span className='text-gray-800 font-bold'> React </span>, and the latest <span className='text-sky-400 font-bold'> Tailwind CSS v4. </span>
+              It includes a clean Home page, an About section, a detailed Skills section, and a fully functional Contact form.
+            </p>
+          <div className='project-buttons'>
+              <a
+              href="https://samsongideyportifolio.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="button px-2 py-2 my-2 max-sm:my-3"> Visit Live Website</button>
+            </a>
+            <a
+              href="https://github.com/smile679/samson_portifolio.git"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            <button className="button3"> Code on Github</button>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
 
     <div className='project-card'>
-      <img src={ movieScope } alt="Movie-Scope App image"/>
-      <div className='project-info'>
         <h3>Movie Scope App</h3>
-          <p className='text-[13px] leading-5 text-white text-pretty'>MovieScope is a modern and responsive web application that allows users to browse, search, 
+      <div className='project-grid'>
+        <div className='flex flex-col justify-evenly space-y-3'>
+          <img src={ movieScope2 } alt="Movie-Scope App image"/>
+          <img src={ movieScope } alt="Movie-Scope App image"/>
+        </div>
+      <div className='project-info'>
+          <p className='text-[1rem] leading-6 text-white text-pretty my-2'>MovieScope is a modern and responsive web application that allows users to browse, search, 
             and discover movies from a wide selection of genres. The project is developed using 
             <span className='text-amber-400 font-bold'> JavaScript</span>, 
             <span className='text-gray-800 font-bold'> React </span>, and the latest 
             <span className='text-sky-400 font-bold'> Tailwind CSS v4. </span>
             <span className='text-gray-800 font-bold'> and React Router </span> it provides a clean and interactive user experience.
           </p>
-        <div className='project-buttons'>
+          <div className='flex flex-col items-start my-2'>
+            <div className='w-full flex justify-between'>
+              <h3>🔥 Features</h3>
+              <button className='text-[0.8rem] text-emerald-400 font-semibold sm:hidden' onClick={()=>setShowText(prev=>({...prev, movie:!prev.movie}))}>{showText.movie ? "Hide Text" : "Show More"}</button>
+            </div>
+              { showText.movie ? <ul className='text-[1.1rem] text-gray-800 font-semibold my-2'>
+                <li>🔍 Search movies by title</li>
+                <li>🎞️ Browse trending and popular movies</li>
+                <li>🧾 View detailed movie info: title, rating, description, poster, and more</li>
+                <li>🛒 Add to cart functionality</li>
+                <li>📱 Fully responsive design for both desktop and mobile devices</li>
+              </ul> : ""}
+          </div>
+          <div className='project-buttons'>
+            <a
+            href="https://moviescopapp.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="button px-2 py-2 my-2 max-sm:my-3"> Visit Live Website</button>
+          </a>
           <a
-          href="https://moviescopapp.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="button px-2 py-2 my-2 max-sm:my-3"> Visit Live Website</button>
-        </a>
-        <a
-          href="https://github.com/smile679/Movie-App.git"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <button className="button3"> Code on Github</button>
-        </a>
+            href="https://github.com/smile679/Movie-App.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          <button className="button3"> Code on Github</button>
+          </a>
+        </div>
       </div>
       </div>
     </div>
 
     <div className='project-card'>
-      <img src={ foodiehome2 } alt="Foodie Delight image"/>
+      <h3>Foodie Delight App</h3>
+      <div className='project-grid'>
+        <div className='flex flex-col justify-evenly space-y-3'>
+        <img src={ foodiehome2 } alt="Foodie Delight image"/>
+        <img src={ foodie2 } alt="Foodie Delight image"/>
+      </div>
       <div className='project-info'>
-        <h3>Foodie Delight App</h3>
-          <p className='text-[13px] leading-5 text-white text-pretty'>MovieScope is a modern and responsive web application that allows users to browse, search, 
-            and discover movies from a wide selection of genres. The project is developed using 
+          <p className='text-[1rem] leading-6 text-white text-pretty my-1'>Foodie Delight is a modern, responsive web application that helps users discover and now order delicious meals from around the world. Whether you're in the mood for pasta, dessert,
+             or something brand new, Foodie Delight lets you search, explore, and order with ease. Build with
             <span className='text-amber-400 font-bold'> JavaScript</span>, 
             <span className='text-gray-800 font-bold'> React </span>, and the latest
             <span className='text-sky-400 font-bold'> Tailwind CSS v4. </span>
             <span className='text-gray-800 font-bold'> and React Router </span> it provides a clean and interactive user experience.
           </p>
-        <div className='project-buttons'>
+          <div className='flex flex-col items-start my-2'>
+            <div className='w-full flex justify-between'>
+              <h3>🔥 Features</h3>
+              <button className='text-[0.8rem] text-emerald-400 font-semibold sm:hidden' onClick={()=>setShowText(prev=>({...prev, foodie:!prev.foodie}))}>{showText.foodie ? "Hide Text" : "Show More"}</button>
+            </div>
+              { showText.foodie ? <ul className='text-[1.1rem] text-gray-800 font-semibold my-2'>
+                <li>🔍 Search any recipe by name</li>
+                <li>🖼️ Clean and responsive recipe card layout</li>
+                <li>📄 Detailed recipe pages with ingredients and instructions</li>
+                <li>🛒 Add to cart functionality</li>
+                <li>🧾 Cart summary and order receipt</li>
+                <li>💳 Order and payment simulation (demo)</li>
+                <li>🌐 Built with React, Tailwind CSS, and React Router</li>
+                <li>📱 Fully responsive design for both desktop and mobile devices</li>
+              </ul> : ""}
+          </div>
+          <div className='project-buttons'>
+            <a
+            href="https://foodiedelightapp.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="button px-2 py-2 max-sm:my-3"> Visit Live Website</button>
+          </a>
           <a
-          href="https://foodiedelightapp.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="button px-2 py-2 max-sm:my-3"> Visit Live Website</button>
-        </a>
-        <a
-          href="https://github.com/smile679/Foodie-Delight.git"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <button className="button3">
-           Code on Github</button>
-        </a>
+            href="https://github.com/smile679/Foodie-Delight.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          <button className="button3">
+            Code on Github</button>
+          </a>
+        </div>
       </div>
       </div>
     </div>
