9a5a99019674d9e983ef60090537c086e13acf6a
Samson <samuelgide88@gmail.com>
2026-07-25 18:04:31 +0300

    project.js  update

--- client/src/pages/home/projectOverView.jsx
+++ client/src/pages/home/projectOverView.jsx
@@ -8,94 +8,10 @@ import Reveal from "../../components/motions/Reveal";
 import BlurText from "../../components/motions/BlurText";
 import Tilt from "react-parallax-tilt";
 import MenuCard from "../../components/MenuCard";
+import { projects } from "../../lib/projects.js"
 
 function ProjectOverView() {
   const [menu, setMenu] = useState("all");
-
-  const projects = [
-    {
-      title: "Gefrem Engineering Website",
-      shortDescription:
-        "The Gefrem Engineering website was developed to establish a strong online presence for an engineering and construction company",
-      image:
-        "https://res.cloudinary.com/dineyc77u/image/upload/v1782205362/gef_01_pjen9p.jpg",
-      link: "https://gefrem-engineering.vercel.app/",
-      github: "https://github.com/smile679/gefrem_Engineering.git",
-      category: "static",
-      tags: ["JavaScript", "React", "React Router", "Tailwind Css"],
-    },
-    {
-      title: "Merkato Gebeya Full-Stack App",
-      shortDescription:
-        "The Merkato eCommerce App was built to offer a smoother, more organized shopping experience for users who often face slow, cluttered, or confusing online stores",
-      image:
-        "https://res.cloudinary.com/dineyc77u/image/upload/v1763652914/nn3_yz6lwo.jpg",
-      link: "https://ecommerce-app-1-q5og.onrender.com/",
-      github: "https://github.com/smile679/eCommerce-app.git",
-      category: "fullstack",
-      tags: [
-        "JavaScript",
-        "React",
-        "Node.js",
-        "Express",
-        "MongoDB",
-        "React Router",
-        "Tailwind",
-      ],
-    },
-    {
-      title: "Bakery Full-Stack App",
-      shortDescription:
-        "The Bakery Fullstack App was built to make it easier for customers to discover, browse, and order baked goods online without navigating multiple platforms",
-      image:
-        "https://res.cloudinary.com/dineyc77u/image/upload/v1763417061/bakerFront_xvptpc.jpg",
-      link: "https://bakery-full-stack-webisite-front.onrender.com/",
-      github: "https://github.com/smile679/Bakery-Full-stack-Webisite.git",
-      category: "fullstack",
-      tags: [
-        "JavaScript",
-        "React",
-        "Node.js",
-        "Express",
-        "MongoDB",
-        "React Router",
-        "Tailwind",
-      ],
-    },
-    {
-      title: "Foodie Delight",
-      shortDescription:
-        "The Bakery Fullstack App was created to give customers and bakery owners a smoother, more organized online experience.",
-      image:
-        "https://res.cloudinary.com/dineyc77u/image/upload/v1763846372/foodiehome2_hlitdu.webp",
-      link: "https://foodiedelightapp.netlify.app/",
-      github: "https://github.com/smile679/Foodie-Delight.git",
-      category: "static",
-      tags: ["JavaScript", "React", "React Router", "Tailwind Css"],
-    },
-    {
-      title: "Movie Scope",
-      shortDescription:
-        "MovieScope is a responsive movie discovery web application that allows users to browse trending films, search titles, and view detailed information",
-      image:
-        "https://res.cloudinary.com/dineyc77u/image/upload/v1763846926/movieScope_vhccly.webp",
-      link: "https://moviescopapp.netlify.app/",
-      github: "https://github.com/smile679/Movie-App.git",
-      category: "static",
-      tags: ["JavaScript", "React", "React Router", "Tailwind Css"],
-    },
-    {
-      title: "Mobile Shop Management App",
-      shortDescription:
-        "Product showcase and admin management system for a mobile phone shop, allowing customers to view available devices while enabling admins to manage inventory.",
-      image:
-        "https://res.cloudinary.com/dineyc77u/image/upload/v1782205772/mobileShop_gzukzk.jpg",
-      link: "https://mobileshop-1mku.onrender.com/",
-      github: "https://github.com/smile679/mobileShop.git",
-      category: "static",
-      tags: ["JavaScript", "React", "React Router", "Tailwind Css"],
-    },
-  ];
 
 
   const filteredProjects =
